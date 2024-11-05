c1c3509c0688202e87940d7a7e3ae5b09948ec94
Mayur Gohane <mayur100888@gmail.com>
2024-11-05 17:49:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,8 @@
-from flask import Flask,render_template,request
+from fastapi import FastAPI
+import uvicorn
 
-app=Flask(__name__)
+app = FastAPI()
 
-@app.route('/',methods=['GET'])
-def welcome():
-    return "Welcome to my website"
-
-@app.route('/index', methods=['GET'])
-def index():
-    return "This is the index page"
-
-#varibale rule
-@app.route('/success/<int:score>')
-def sucess(score):
-    return "Congratulations, you scored "+str(score)
-
-@app.route('/fail/<int:score>')
-def fail(score):
-    return 'Person failed'+str(score)
-
-@app.route('/index',methods=['GET','POST'])
-def form():
-    if request.method == 'GET':
-        return render_template('form.html')
-
-if __name__=="__main__":
-    app.run(debug=True)
+@app.get('/')
+def getdata():
+    return "Hello Mayur"
